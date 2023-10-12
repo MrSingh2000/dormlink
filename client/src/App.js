43b001c85d69775c584a815d59b024b7afc433b9
Anshuman Singh <www.anshu2000@gmail.com>
2023-10-12 14:03:45 +0530

    initialized redux and router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+
+const router = createBrowserRouter([
+  { path: "/", Component: Home },
+  { path: "*", Component: Root },
+]);
 
 function App() {
+  return;
+  <RouterProvider router={router} />;
+}
+
+function Root() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 }
 
