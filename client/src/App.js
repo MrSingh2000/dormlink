ba57d2c269dd4f16aaa1bff008f3f86d9936c9ee
Kajal Aggarwal <aggarwalujjwal20@gmail.com>
2023-11-30 15:02:59 +0530

    ui

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <AdminDashboard/>
+      <Footer/>
     </div>
   );
 }
--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -4,31 +4,15 @@ function AdminDashboard() {
   return (
     <div>
     <ul className=" grid grid-col divide-y divide-gray-500">
-       <li className="pb-3 sm:pb-4">
-          <div className="flex items-center  rtl:space-x-reverse">
-             <div className="flex-shrink-0">
-               <image  style={{height:'10vh'}} className=" rounded-full" src={avatar} alt="Student image"/>
-             </div>
-             <div className="flex-1 min-w-0">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                   Student Name
-                </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                   Roll Number
-                </p>
-             </div>
-             <div className="flex-1 min-w-0">
-                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                   Student Name
-                </p>
-                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                   Roll Number
-                </p>
-             </div>
-             <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                $320
-             </div>
-          </div>
+       <li>
+        <div><img className="round-full" alt='student' src={avatar}/></div>
+        <div><p>Student Name</p>
+        <p>Roll Nu</p>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
        </li>
     </ul>
     </div>
